d6f2acaccb623b8df73ed6890fd9d6c7c5f5ba85
Michael Toohig <mtoohig@redventures.com>
2024-10-02 16:55:42 -0400

    Add lazygit to nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,10 @@
 
 -- Set conceal level to 0
 vim.o.conceallevel = 0
+
+-- Lazygit
+vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
+-- TODO: investigate neovim-remote to allow editing commit messages in the neovim instance instead of a nano editor
+-- vim.g.lazygit_use_neovim_remote = 1
